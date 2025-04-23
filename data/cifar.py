a558b524722f399ff94b4aacc72d13014d9945aa
unclezalman <160236291+unclezalman@users.noreply.github.com>
2025-04-23 23:49:59 +0800

    Update cifar.py

--- data/cifar.py
+++ data/cifar.py
@@ -1,24 +1,21 @@
 from torchvision.datasets import CIFAR10, CIFAR100
 from copy import deepcopy
 import numpy as np
+import clip
 
 from data.data_utils import subsample_instances
 from config import cifar_10_root, cifar_100_root
 
-
 class CustomCIFAR10(CIFAR10):
-
     def __init__(self, *args, **kwargs):
-
         super(CustomCIFAR10, self).__init__(*args, **kwargs)
-
         self.uq_idxs = np.array(range(len(self)))
+        self.class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+                           'dog', 'frog', 'horse', 'ship', 'truck']
 
     def __getitem__(self, item):
-
         img, label = super().__getitem__(item)
         uq_idx = self.uq_idxs[item]
-
         return img, label, uq_idx
 
     def __len__(self):
@@ -26,83 +23,86 @@ class CustomCIFAR10(CIFAR10):
 
 
 class CustomCIFAR100(CIFAR100):
-
     def __init__(self, *args, **kwargs):
         super(CustomCIFAR100, self).__init__(*args, **kwargs)
-
         self.uq_idxs = np.array(range(len(self)))
+        self.class_names = [f'class_{i}' for i in range(100)]
 
     def __getitem__(self, item):
         img, label = super().__getitem__(item)
         uq_idx = self.uq_idxs[item]
-
         return img, label, uq_idx
 
     def __len__(self):
         return len(self.targets)
 
 
+class TextPromptCIFAR(CIFAR100):
+    def __init__(self, base_dataset, text_prompts=None):
+        self.data = base_dataset.data
+        self.targets = base_dataset.targets
+        self.transform = base_dataset.transform
+        self.uq_idxs = base_dataset.uq_idxs
+        self.class_names = base_dataset.class_names
+        self.text_prompts = text_prompts or [
+            "a photo of a {}",
+            "a cropped photo of a {}",
+            "a good photo of a {}",
+            "a bad photo of a {}",
+            "a photo of one {}",
+            "a photo of many {}"
+        ]
+        
+    def __getitem__(self, index):
+        image, label, uq_idx = super().__getitem__(index)
+        class_name = self.class_names[label]
+        text_inputs = [clip.tokenize(prompt.format(class_name))[0] for prompt in self.text_prompts]
+        return image, label, text_inputs, uq_idx
+
+    def __len__(self):
+        return len(self.targets)
+
+
 def subsample_dataset(dataset, idxs):
-
-    # Allow for setting in which all empty set of indices is passed
-
     if len(idxs) > 0:
-
         dataset.data = dataset.data[idxs]
         dataset.targets = np.array(dataset.targets)[idxs].tolist()
         dataset.uq_idxs = dataset.uq_idxs[idxs]
-
         return dataset
-
     else:
-
         return None
 
 
 def subsample_classes(dataset, include_classes=(0, 1, 8, 9)):
-
     cls_idxs = [x for x, t in enumerate(dataset.targets) if t in include_classes]
-
     target_xform_dict = {}
     for i, k in enumerate(include_classes):
         target_xform_dict[k] = i
-
     dataset = subsample_dataset(dataset, cls_idxs)
-
-    # dataset.target_transform = lambda x: target_xform_dict[x]
-
     return dataset
 
 
 def get_train_val_indices(train_dataset, val_split=0.2):
-
     train_classes = np.unique(train_dataset.targets)
-
-    # Get train/test indices
     train_idxs = []
     val_idxs = []
     for cls in train_classes:
-
         cls_idxs = np.where(train_dataset.targets == cls)[0]
-
         v_ = np.random.choice(cls_idxs, replace=False, size=((int(val_split * len(cls_idxs))),))
         t_ = [x for x in cls_idxs if x not in v_]
-
         train_idxs.extend(t_)
         val_idxs.extend(v_)
-
     return train_idxs, val_idxs
 
 
 def get_cifar_10_datasets(train_transform, test_transform, train_classes=(0, 1, 8, 9),
                        prop_train_labels=0.8, split_train_val=False, seed=0):
-
     np.random.seed(seed)
 
     # Init entire training set
     whole_training_set = CustomCIFAR10(root=cifar_10_root, transform=train_transform, train=True, download=True)
 
-    # Get labelled training set which has subsampled classes, then subsample some indices from that
+    # Get labelled training set
     train_dataset_labelled = subsample_classes(deepcopy(whole_training_set), include_classes=train_classes)
     subsample_indices = subsample_instances(train_dataset_labelled, prop_indices_to_subsample=prop_train_labels)
     train_dataset_labelled = subsample_dataset(train_dataset_labelled, subsample_indices)
@@ -120,9 +120,11 @@ def get_cifar_10_datasets(train_transform, test_transform, train_classes=(0, 1, 
     # Get test set for all classes
     test_dataset = CustomCIFAR10(root=cifar_10_root, transform=test_transform, train=False, download=True)
 
-    # Either split train into train and val or use test set as val
-    train_dataset_labelled = train_dataset_labelled_split if split_train_val else train_dataset_labelled
-    val_dataset_labelled = val_dataset_labelled_split if split_train_val else None
+    # Wrap datasets with TextPromptCIFAR
+    train_dataset_labelled = TextPromptCIFAR(train_dataset_labelled_split if split_train_val else train_dataset_labelled)
+    train_dataset_unlabelled = TextPromptCIFAR(train_dataset_unlabelled)
+    val_dataset_labelled = TextPromptCIFAR(val_dataset_labelled_split) if split_train_val else None
+    test_dataset = TextPromptCIFAR(test_dataset)
 
     all_datasets = {
         'train_labelled': train_dataset_labelled,
@@ -136,13 +138,12 @@ def get_cifar_10_datasets(train_transform, test_transform, train_classes=(0, 1, 
 
 def get_cifar_100_datasets(train_transform, test_transform, train_classes=range(80),
                        prop_train_labels=0.8, split_train_val=False, seed=0):
-
     np.random.seed(seed)
 
     # Init entire training set
     whole_training_set = CustomCIFAR100(root=cifar_100_root, transform=train_transform, train=True, download=True)
 
-    # Get labelled training set which has subsampled classes, then subsample some indices from that
+    # Get labelled training set
     train_dataset_labelled = subsample_classes(deepcopy(whole_training_set), include_classes=train_classes)
     subsample_indices = subsample_instances(train_dataset_labelled, prop_indices_to_subsample=prop_train_labels)
     train_dataset_labelled = subsample_dataset(train_dataset_labelled, subsample_indices)
@@ -160,9 +161,11 @@ def get_cifar_100_datasets(train_transform, test_transform, train_classes=range(
     # Get test set for all classes
     test_dataset = CustomCIFAR100(root=cifar_100_root, transform=test_transform, train=False, download=True)
 
-    # Either split train into train and val or use test set as val
-    train_dataset_labelled = train_dataset_labelled_split if split_train_val else train_dataset_labelled
-    val_dataset_labelled = val_dataset_labelled_split if split_train_val else None
+    # Wrap datasets with TextPromptCIFAR
+    train_dataset_labelled = TextPromptCIFAR(train_dataset_labelled_split if split_train_val else train_dataset_labelled)
+    train_dataset_unlabelled = TextPromptCIFAR(train_dataset_unlabelled)
+    val_dataset_labelled = TextPromptCIFAR(val_dataset_labelled_split) if split_train_val else None
+    test_dataset = TextPromptCIFAR(test_dataset)
 
     all_datasets = {
         'train_labelled': train_dataset_labelled,
@@ -175,7 +178,6 @@ def get_cifar_100_datasets(train_transform, test_transform, train_classes=range(
 
 
 if __name__ == '__main__':
-
     x = get_cifar_100_datasets(None, None, split_train_val=False,
                          train_classes=range(80), prop_train_labels=0.5)
 
@@ -193,3 +195,4 @@ if __name__ == '__main__':
     print(f'Num Unabelled Classes: {len(set(x["train_unlabelled"].targets))}')
     print(f'Len labelled set: {len(x["train_labelled"])}')
     print(f'Len unlabelled set: {len(x["train_unlabelled"])}')
+    
